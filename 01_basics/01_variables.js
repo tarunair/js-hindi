ed30885255443f4683915aac4a9892739a650aa5
tarunair <167167593+tarunair@users.noreply.github.com>
2024-12-30 15:08:15 +0000

    confirmed changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail = "Tarun@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+
+//accountId = 2 // not allowed
+
+console.table([accountId, accountEmail, accountPassword, accountCity]); 
